d024741f0d3a9330ea8cb2421a0102a2b3a6aa87
Jack Mead <jack-mead@hotmail.co.uk>
2022-02-12 11:05:22 +0000

    Basic local build up & running

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import Fake from "src/sub_module/test";
+import { MY_ENV_VARIABLE } from "env";
+
+const App = () => {
+  return (
+    <h1>
+      This is my React app!
+      {MY_ENV_VARIABLE}
+      <Fake />
+    </h1>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/sub_module/test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Fake = () => {
+  return <h2>Testing231211</h2>;
+};
+
+export default Fake;
